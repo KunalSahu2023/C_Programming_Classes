bc4de17df4983702bbead20b4325f3087ff30f11
Kunal Sahu. <123799743+KunalSahu2023@users.noreply.github.com>
2024-12-28 21:51:06 +0000

    .

--- BasicsCProgram/5. CalculateAreaCircumference.c
+++ BasicsCProgram/5. CalculateAreaCircumference.c
@@ -1,13 +1,15 @@
 
-// Calculate the Area of a Circle.
+// Calculate the Area and Circumference of a Circle.
 #include <stdio.h>
 int main()
 {
-    float PI = 3.14, area;
+    float PI = 3.14, area,circum;
     int r;
     printf("Enter the value of r \n");
     scanf("%d", &r);
     area = PI * r * r;
-    printf("Area of Circle is %.2f", area);
+    printf("Area of Circle is %.2f\n", area);
+    circum = 2 * PI * r ;
+    printf("Circumference of Circle is %.2f", circum);
     return 0;
 }
