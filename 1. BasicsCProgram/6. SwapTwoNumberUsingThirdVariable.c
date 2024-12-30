fab0543209131bb1c655df7ea160de7bebbb4389
Kunal Sahu. <123799743+KunalSahu2023@users.noreply.github.com>
2024-12-31 02:33:00 +0530

    .

---
+++ 1. BasicsCProgram/6. SwapTwoNumberUsingThirdVariable.c
@@ -0,0 +1,16 @@
+
+// Using Third Variable 
+
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter the value of and b \n");
+    scanf("%d %d", &a, &b);
+    printf("Value of a and b Before Swapping is %d %d", a, b);
+    c = a;
+    a = b;
+    b = c;
+    printf("\nValue of a and b After Swapping is %d %d", a, b);
+    return 0;
+}
